283d43aca641e0b0e44106cd523a7006b8065316
Richie <yourfriend2428@gmail.com>
2024-11-05 00:19:21 +0800

    - Added functions to check if certain storage files exist - Committed blank storage files

--- main_menu.py
+++ main_menu.py
@@ -1,41 +1,111 @@
-print("============================================")  # Prints the selection menu for the UMS
-print("Welcome to the University Management System!")
-print("============================================")
-print("")
-print("Are you a(n): ")
-print("")
-print("1) Administrator")
-print("2) Lecturer")
-print("3) Student")
-print("4) Registrar")
-print("5) Accountant")
-print("")
-print("Enter 6 if you wish to exit the program.")
-print("")
-
-choice = 0  # Initializes a variable named choice to receive user input
-while True:  # Repeats infinitely unless a valid choice is given
+def check_courses_file():  # Checks if courses.txt exists, if no, it creates a new file
+    courses_filepath = "courses.txt"
     try:
-        choice = int(input("Enter your choice (1/2/3/4/5/6): "))  # Gets the user's input
-        if choice in [1, 2, 3, 4, 5, 6]:  # Checks if the user input is valid
-            break  # Stops the while loop
-        else:
+        file = open(courses_filepath, "r")
+        file.close()
+    except FileNotFoundError:
+        file = open(courses_filepath, "x")
+        file.close()
+
+
+def check_students_file():  # Checks if students.txt exists, if no, it creates a new file
+    students_filepath = "students.txt"
+    try:
+        file = open(students_filepath, "r")
+        file.close()
+    except FileNotFoundError:
+        file = open(students_filepath, "x")
+        file.close()
+
+
+def check_lecturers_file():  # Checks if students.txt exists, if no, it creates a new file
+    lecturers_filepath = "lecturers.txt"
+    try:
+        file = open(lecturers_filepath, "r")
+        file.close()
+    except FileNotFoundError:
+        file = open(lecturers_filepath, "x")
+        file.close()
+
+
+def check_modules_file():  # Checks if modules.txt exists, if no, it creates a new file
+    modules_filepath = "modules.txt"
+    try:
+        file = open(modules_filepath, "r")
+        file.close()
+    except FileNotFoundError:
+        file = open(modules_filepath, "x")
+        file.close()
+
+
+def check_fees_files():  # Checks if fees.txt exists, if no, it creates a new file
+    fees_filepath = "fees.txt"
+    try:
+        file = open(fees_filepath, "r")
+        file.close()
+    except FileNotFoundError:
+        file = open(fees_filepath, "x")
+        file.close()
+
+
+def check_grades_file():  # Checks if grades.txt exists, if no, it creates a new file
+    grades_filepath = "grades.txt"
+    try:
+        file = open(grades_filepath, "r")
+        file.close()
+    except FileNotFoundError:
+        file = open(grades_filepath, "x")
+        file.close()
+
+
+def main():
+    check_students_file()
+    check_fees_files()
+    check_courses_file()
+    check_grades_file()
+    check_modules_file()
+    check_lecturers_file()
+
+    print("============================================")  # Prints the selection menu for the UMS
+    print("Welcome to the University Management System!")
+    print("============================================")
+    print("")
+    print("Are you a(n): ")
+    print("")
+    print("1) Administrator")
+    print("2) Lecturer")
+    print("3) Student")
+    print("4) Registrar")
+    print("5) Accountant")
+    print("")
+    print("Enter 6 if you wish to exit the program.")
+    print("")
+
+    while True:  # Repeats infinitely unless a valid choice is given
+        try:
+            choice = int(input("Enter your choice (1/2/3/4/5/6): "))  # Gets the user's input
+            if choice in [1, 2, 3, 4, 5, 6]:  # Checks if the user input is valid
+                break  # Stops the while loop
+            else:
+                print("You need to enter a value between 1 and 6!")
+                print("")
+        except ValueError:  # Exception handling in case a non-integer value is inputted
             print("You need to enter a value between 1 and 6!")
             print("")
-    except ValueError:  # Exception handling in case a non-integer value is inputted
-        print("You need to enter a value between 1 and 6!")
-        print("")
 
-match choice:
-    case 1:
-        login_admin()  # Login page for administrators
-    case 2:
-        login_lecturer()  # Login page for lecturers
-    case 3:
-        login_student()  # Login page for students
-    case 4:
-        login_registar()  # Login page for Registrar
-    case 5:
-        login_accountant()  # Login page for accountants
-    case 6:
-        exit()  # Exits the program
+    match choice:
+        case 1:
+            login_admin()  # Login page for administrators
+        case 2:
+            login_lecturer()  # Login page for lecturers
+        case 3:
+            login_student()  # Login page for students
+        case 4:
+            login_registar()  # Login page for Registrar
+        case 5:
+            login_accountant()  # Login page for accountants
+        case 6:
+            exit()  # Exits the program
+
+
+main()
